97aa19a14158392426a0f9a0aa1bb878835d12cd
GIS Jammerbugt <gis@jammerbugt.dk>
2015-04-20 15:35:05 +0200

    Update main.js

--- routeplanner/js/main.js
+++ routeplanner/js/main.js
@@ -131,7 +131,7 @@
                         '<br>' +
                         value.request.destination +
                         '<br>' +
-                        'Befordringspris: ' + (Math.round(config.befording * (value.leg.distance.value/1000))) + ' kr.' +
+                        'Befordringspris: ' + <b>(Math.round(config.befording * (value.leg.distance.value/1000))) + ' kr.' </b> +
                         '<br>' +
                         '<div class="rejseplan-link" id="takst' + index + '"></div>' +
                         '</p>' +
