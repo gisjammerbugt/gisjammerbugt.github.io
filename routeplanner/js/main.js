e32eac55b7fde5159787c5c4bd7f96efc4b9948e
GIS Jammerbugt <gis@jammerbugt.dk>
2017-09-05 10:20:23 +0200

    Update main.js

--- routeplanner/js/main.js
+++ routeplanner/js/main.js
@@ -132,7 +132,7 @@
                         '<div class="number">' + (index + 1) + '</div>' +
                         '<h4 class="list-group-item-heading">' +
                         value.leg.distance.text +
-                        ' (Google)    ' +
+                        ' (Google)    - ' +
                         value.leg.distance.text +
                         ' (KRAK)' +
                         '</h4>' +
