3b4300efb8ec3f12a7702f4aad3393c1f47e861a
GIS Jammerbugt <gis@jammerbugt.dk>
2018-01-02 14:09:25 +0100

    Update config.js

--- routeplanner/js/config.js
+++ routeplanner/js/config.js
@@ -1,4 +1,4 @@
 var config = {
-    befording: {"_2015": 2.05, "_2016": 1.99, "_2017": 1.93, "_2018": 1.93},
+    befording: {"_2015": 2.05, "_2016": 1.99, "_2017": 1.93, "_2018": 1.94, "_2019": 1.94},
     rejseplanenAPI: "http://xmlopen.rejseplanen.dk/bin/rest.exe"
 };
